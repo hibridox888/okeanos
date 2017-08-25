870e62dd6713679de0f1ef60610b4e67da91d459
user0able <hfontanezp@hotmail.com>
2017-08-25 01:17:34 -0300

    add apps
    add aplication: blog, contact base

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contacto
+
+
+class ContactoAdmin(admin.ModelAdmin):
+    model = Contacto
+    search_fields = ['nombre', 'email', 'mensaje']
+    list_filter = ['fecha', ]
+    readonly_fields = ['fecha', ]
+    ordering = ['fecha']
+
+
+admin.site.register(Contacto, ContactoAdmin)
+
+
